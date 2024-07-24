02578bfcbfe014647376df064488086c3cf125f4
Nagyvee <nigelmafuta1@gmail.com>
2024-07-24 23:20:15 +0200

    save

--- server/app.js
+++ server/app.js
@@ -11,7 +11,7 @@ app.use(bodyParser.json());
 
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://nayveechatbot.onrender.com"],
+    origin: ["http://localhost:5173", "https://nayveechatbot.onrender.com/"],
     credentials: true,
   })
 );
