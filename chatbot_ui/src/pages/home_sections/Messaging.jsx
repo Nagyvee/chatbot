b35acd90bb223be0079a54c365e2d12e9d001665
Nagyvee <nigelmafuta1@gmail.com>
2024-07-29 16:47:33 +0200

    messaging styles

--- chatbot_ui/src/pages/home_sections/Messaging.jsx
+++ chatbot_ui/src/pages/home_sections/Messaging.jsx
@@ -5,7 +5,7 @@ import profileIcon from "../../assets/profile.jpg";
 import ReactMarkdown from "react-markdown";
 import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
 import { materialDark } from "react-syntax-highlighter/dist/esm/styles/prism";
-import useTypewriter from "./TypeWriter"; 
+import useTypewriter from "./TypeWriter";
 import { CopyToClipboard } from "react-copy-to-clipboard";
 import { FaCopy } from "react-icons/fa";
 import NayveeIcon from "../../assets/nayvee_logo_ icon_nobg.png";
@@ -18,58 +18,62 @@ const Container = styled.div`
   border-radius: 8px;
   overflow-y: auto;
 
-  .loading{
-  display: flex;
-  margin: 0 auto;
-  font-weight: 350;
+  @media(max-width: 485px){
+  padding: 2rem 0;
+  }
 
-    img{
-    width: 22px;
-    height: 22px;
-    margin:0 0.45rem;
-    animation: spin 1s infinite linear;
+  .loading {
+    display: flex;
+    margin: 0 auto;
+    font-weight: 350;
 
-     @keyframes spin{
-  from{
-  trasform: rotate(0deg)
-  }
-  to{
-  transform: rotate(360deg)
-  }
-  }
-}
+    img {
+      width: 22px;
+      height: 22px;
+      margin: 0 0.45rem;
+      animation: spin 1s infinite linear;
 
-span{
-  animation: textSize 3s infinite linear;
-  transition: animation ease-in;
+      @keyframes spin {
+        from {
+          transform: rotate(0deg);
+        }
+        to {
+          transform: rotate(360deg);
+        }
+      }
+    }
 
-    @keyframes textSize{
-  from{
-  font-size: .5rem;
-  }
-  to{
-    font-size: .85rem;
-  }
-  }
-}
+    span {
+      animation: textSize 3s infinite linear;
+      transition: animation ease-in;
+
+      @keyframes textSize {
+        from {
+          font-size: 0.5rem;
+        }
+        to {
+          font-size: 0.85rem;
+        }
+      }
+    }
   }
 `;
 
 const Message = styled.div`
   display: flex;
-  flex-wrap: ${({sender}) => sender === 'user' ?"no-wrap" : "wrap"};
+  flex-wrap: ${({ sender }) => (sender === "user" ? "no-wrap" : "wrap")};
   align-items: flex-start;
   position: relative;
-  padding:.75rem 1.5rem .2rem .5rem;
+  padding: 0.75rem 1.5rem 0.2rem 0.5rem;
   background-color: ${({ sender }) =>
     sender === "user" ? "#A3FFD6" : "#f9f9f9"};
   border-radius: 2px;
   box-shadow: 0 1px 2px rgba(0, 0, 0, 0.1);
   max-width: 75%;
-   ${({ sender })=> sender === "user" && "align-self: flex-end;"}
+  ${({ sender }) => sender === "user" && "align-self: flex-end;"}
 
   @media (max-width: 768px) {
-      max-width: ${({sender}) => sender ==='user'? "80%": "100%"};
+    max-width: ${({ sender }) => (sender === "user" ? "80%" : "100%")};
   }
 
   .user-profile {
@@ -96,6 +100,8 @@ const MarkdownContent = styled.div`
   font-size: 1rem;
   color: #333;
   width: 100%;
+  position: relative;
+
 
   h1,
   h2,
@@ -114,12 +120,26 @@ const MarkdownContent = styled.div`
     overflow-x: auto;
     position: relative;
     background-color: #2d2d2d;
+    white-space: pre-wrap;
+    word-wrap: break-word;
+
+    @media (max-width: 378px) {
+      font-size: 0.75rem;
+      padding: 0;
+    }
+  }
   }
 
   code {
     background: #f5f5f5;
-    padding: 0.2rem 0.4rem;
+    padding: .3rem 0.4rem;
     border-radius: 3px;
+    white-space: pre-wrap;
+    word-wrap: break-word;
+
+        @media (max-width: 487px) {
+      // font-size: 0.875rem;
+    }
   }
 
   blockquote {
@@ -148,7 +168,7 @@ const CopyButton = styled.button`
   position: absolute;
   top: 0.5rem;
   right: 0.5rem;
-  background: ${({copie}) => copie ? "#888" : "#0366d6"};
+  background: ${({ copie }) => (copie ? "#888" : "#0366d6")};
   color: white;
   border: none;
   padding: 0.25rem;
@@ -161,7 +181,7 @@ const CopyButton = styled.button`
   z-index: 2; /* Ensure it's on top */
 
   &:hover {
-    background: ${({copie}) => copie ? "#888" : "#024b9a"};
+    background: ${({ copie }) => (copie ? "#888" : "#024b9a")};
   }
 `;
 
@@ -195,7 +215,13 @@ const CodeBlock = ({ className, children }) => {
           {copyText ? "Copied" : "Copy"}
         </CopyButton>
       </CopyToClipboard>
-      <SyntaxHighlighter style={materialDark} language={language} PreTag="pre">
+      <SyntaxHighlighter
+        style={materialDark}
+        language={language}
+        PreTag="pre"
+        wrapLines={true}
+        wrapLongLines={true}
+      >
         {String(children).replace(/\n$/, "")}
       </SyntaxHighlighter>
     </div>
@@ -218,7 +244,7 @@ const components = {
 
 const ChatMessage = ({ sender, message, user }) => {
   const chatAnimated = useSelector((state) => state.chat.chatAnimated);
-  const typedMessage = sender === "Nayvee"? useTypewriter(message) : message;
+  const typedMessage = sender === "Nayvee" ? useTypewriter(message) : message;
 
   return (
     <Message
@@ -234,17 +260,21 @@ const ChatMessage = ({ sender, message, user }) => {
       )}
       {sender === "Nayvee" ? (
         <MarkdownContent>
-          <ReactMarkdown components={components}>{
-          chatAnimated? typedMessage : message
-          }</ReactMarkdown>
+          <ReactMarkdown components={components}>
+            {chatAnimated ? typedMessage : message}
+          </ReactMarkdown>
         </MarkdownContent>
       ) : (
         <p>{typedMessage}</p>
       )}
       {sender === "Nayvee" && (
         <>
-        <img src={NayveeIcon} alt="Nayvee Logo" className="nayvee-logo" />
-        <span style={{fontSize: "12px", paddingTop:".35rem", color: "#999"}}>Nayvee</span>
+          <img src={NayveeIcon} alt="Nayvee Logo" className="nayvee-logo" />
+          <span
+            style={{ fontSize: "12px", paddingTop: ".35rem", color: "#999" }}
+          >
+            Nayvee
+          </span>
         </>
       )}
     </Message>
