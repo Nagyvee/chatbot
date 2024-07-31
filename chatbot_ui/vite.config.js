895082db574b45e3c3e48abe6222267cb3a6e8a2
Nagyvee <nigelmafuta1@gmail.com>
2024-07-31 03:50:17 +0200

    save pwa for web app

--- chatbot_ui/vite.config.js
+++ chatbot_ui/vite.config.js
@@ -1,7 +1,30 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Nayvee Chatbot App',
+        short_name: 'Nayvee Chat',
+        description: 'Nayvee Chatbot an AI assistant using latest gpt model.',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: '/android-chrome-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '/android-chrome-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ]
+});
