0dd3d72671775395c929e2584ac74b0bc7bcf564
Nagyvee <nigelmafuta1@gmail.com>
2024-08-06 23:37:22 +0200

    save

--- server/controllers/chatController.js
+++ server/controllers/chatController.js
@@ -5,17 +5,15 @@ const chatController = async (req, res) => {
   const { userId, history, sender, message, chatId } = req.body;
 
   try {
-    if (history.length === 0 || history === undefined) {
-      const chatQuery = `INSERT INTO chatbot_chats(id,topic) VALUES(?,?)`;
-      const userChatsQuery = `INSERT INTO user_chats(user_id,chat_id) VALUES(?,?)`;
-
-      await pool.promise().query(chatQuery, [chatId, message]);
-      await pool.promise().query(userChatsQuery, [userId, chatId]);
-    }
-
     const addMessageQuery = `INSERT INTO chatbot_messages(chat_id,content, sender) VALUES(?,?,?)`;
 
     const data = await fetchOpenAIResponse(history, message);
+   if (history.length === 0 || history === undefined) {
+      const chatQuery = `INSERT INTO chatbot_chats(id,topic) VALUES(?,?)`;
+      const userChatsQuery = `INSERT INTO user_chats(user_id,chat_id) VALUES(?,?)`;
+      await pool.promise().query(chatQuery, [chatId, message]);
+      await pool.promise().query(userChatsQuery, [userId, chatId]);
+    }
     await pool.promise().query(addMessageQuery, [chatId, message, sender]);
     await pool.promise().query(addMessageQuery, [chatId, data, "Nayvee"]);
     res.status(200).json(data);
